f393341a59cdf78df6e938732b10cbcb2247cbc6
Alexandr <guk.zverolis@yandex.ru>
2023-02-12 16:01:11 +0300

    Homework4 result_1 commit

--- Homework4/Task2.py
+++ Homework4/Task2.py
@@ -14,7 +14,7 @@
 
 import random
 bush = int(input('Введите количество кустов на грядке: '))
-harvest = [random.randint(1, 10) for _ in range(bush)]
+harvest = [random.randint(1, 20) for _ in range(bush)]
 maxHarvest = 1
 summHarvest = 0
 numBush = ''
@@ -24,7 +24,6 @@ for i in range(len(harvest)):
     first, last = 0, 0
     if summHarvest > maxHarvest:
         maxHarvest = summHarvest
-        print(maxHarvest)
     if i == 0:
         first = len(harvest) - 1
         last = i + 1
@@ -36,63 +35,8 @@ for i in range(len(harvest)):
         last = i + 1
 
     summHarvest = harvest[first] + harvest[i] + harvest[last]
-    numBush = [first, i, last]
+    numBush = [first + 1, i + 1, last + 1]
 
-print(numBush)
-print(maxHarvest)
-
+print(f'Максимальный урожай будет {maxHarvest} с кустов {numBush}')
 
 
-
-
-
-
-
-
-
-
-
-
-"""
-harvest = [1, 1, 1, 4, 1]
-#if bush < 3:
-#    print('Количество не сожет быть меньше 3.')
-#else:
-#    print(harvest)
-
-harv = tuple(harvest)
-maxHarvest = 0
-numberBush = 0
-leftBush = 0
-rightBush = 0
-
-
-print(harv)
-
-for i in range(len(harvest) + 2):
-    summHarvest = harvest[0] + harvest[1] + harvest[2]
-    harvest.append(harvest.pop(0))
-    #print(summHarvest)
-    if summHarvest > maxHarvest:
-        maxHarvest = summHarvest
-        numberBush = i + 1
-        if numberBush > len(harvest) - 1:
-            numberBush = 0
-leftBush = numberBush
-rightBush = numberBush + 2
-if leftBush < 0:
-    leftBush = len(harvest) - numberBush
-if rightBush > len(harvest) - 1:
-    rightBush = rightBush - len(harvest)
-print(f'Максимальный урожай {maxHarvest} ягод при сборе с кустов №№ {leftBush}, {numberBush + 1}, {rightBush}')
-
-
-
-key = 1
-for key in range(1, bush + 1):
-    harv[key] = random.randint(1, 10)
-print(harv)
-#print(*harv.values())
-harv_list = list(harv.values())
-print(harv_list)
-"""
