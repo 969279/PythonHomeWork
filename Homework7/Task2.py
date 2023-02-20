e8ba952ea4eac9f4f7e000c247a758ceeb8ba1a3
Alexandr <guk.zverolis@yandex.ru>
2023-02-20 18:31:27 +0300

    Homework7 task2 commit

--- Homework7/Task2.py
+++ Homework7/Task2.py
@@ -14,3 +14,25 @@
 # 5        10        15         20        25         30
 # 6        12        18         24        30         36
 
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for x in range(1, num_rows + 1):
+        for y in range(1, num_columns + 1):
+            print("%2d" % operation(x, y), end="  ")
+        print()
+
+print_operation_table(lambda x, y: x * y)
+
+
+
+#num_rows = 6
+#num_columns = 6
+
+#for x in range(1, num_rows + 1):
+    #print([x * y for y in range(1, num_columns + 1)])
+#    for y in range(1, num_columns + 1):
+#        print(x * y, end=" ")
+#    print()
+
+
