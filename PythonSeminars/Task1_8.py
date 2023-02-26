e528c3e091dd58e649051fd0cf15e7eba531d1ba
Alexandr <guk.zverolis@yandex.ru>
2023-02-26 15:30:28 +0300

    test_fone commit

--- PythonSeminars/Task1_8.py
+++ PythonSeminars/Task1_8.py
@@ -16,31 +16,94 @@
 # 7. Удалить контакт
 # 8. Выход
 
-menu = {1:'Открыть файл', 2:'Сохранить файл', 3:'Показать контакты',
-        4:'Добавить контакт', 5:'Изменить контакт', 6:'Найти контакт',
-        7:'Удалить контакт', 8: 'Выход'}
+# menu = {1:'Открыть файл', 2:'Сохранить файл', 3:'Показать контакты',
+#         4:'Добавить контакт', 5:'Изменить контакт', 6:'Найти контакт',
+#         7:'Удалить контакт', 8: 'Выход'}
 
-for key, value in menu.items():
-    print(key, value)
+# имя файла
+# file_name = "messages.txt"
+# определяем пустой список
+# messages = list()
 
-#num = int(input('Выберите пункт меню: '))
+# for i in range(4):
+#         message = input("Введите строку " + str(i + 1) + ": ")
+#         messages.append(message + "\n")
 
-#data = open('tel_dir.txt', 'a')
-#with open('tel_dir.txt', 'a') as file:
-#    data.write(key, value for key, value in menu.items())
+# запись списка в файл
+# with open(file_name, "a", encoding='utf-8') as file:
+#         for message in messages:
+#                 file.write(message)
 
-name_list_1 = {'Berlin': '1234'}
-name_list_2 = {'Oslo': '2345'}
+# считываем сообщения из файла
+content = []
+def main_menu():
+        print('Телефонный справочник. Выберите действие: \n'
+        '1. Открыть файл\n'
+        '2. Сохранить файл\n'
+        '3. Показать контакты\n'
+        '4. Добавить контакт\n'
+        '5. Изменить контакт\n'
+        '6. Найти контакт\n' 
+        '7. Удалить контакт\n'
+        '8. Выход')
+        action = int(input('Введите номер действия: '))
+        return action
 
+while True:
+        choice = main_menu()
+        match choice:
+                case 1:# Открыть файл
+                        with open('text.txt', 'r', encoding='utf-8') as text:
+                                content = text.readlines()
+                                print(content)
 
-#with open('listfile.txt', 'w') as filehandle:
-  #  filehandle.writelines(f'{name_list_1}\n')
-  #  filehandle.writelines(f'{name_list_2}\n')
- #   filehandle.readlines()
-data_read = (open('listfile.txt', 'r'))
-print(type(data_read))
+                case 2:# Сохранить файл
+                        if len(content) == 0:
+                                print('Требуется сначала открыть файл.')
+                        else:
+                                with open('text.txt', 'w', encoding='utf-8') as text:
+                                        for item in content:
+                                                text.write(str(item))
 
-#for line in data_read:
-  #  print(line)
-# for key, value in data_read.item():
-#     print(key, value)
+                case 3:# Показать контакты
+                        with open('text.txt', 'r', encoding='utf-8') as text:
+                                content = text.readlines()
+                                for line in content:
+                                        line.split(',')
+                                        print(line)
+
+                case 4:# Добавить контакт
+                        name = input('Введите имя: ')
+                        fone = input('Введите номер телефона: ')
+                        note = input('Введите примечание к номеру телефона: ')
+                        message = [name, fone, note]
+                        with open('text.txt', 'a', encoding='utf-8') as text:
+                                 text.write(', '.join(message) + '\n')
+
+                case 5:# Изменить контакт
+                        name = input('Введите имя: ')
+                        with open('text.txt', 'r', encoding='utf-8') as text:
+                                content = text.readlines()
+                                for i in range(len(content)):
+                                        line = content[i].split(',')
+                                        if line[0].strip() == name:
+                                                print(content[i])
+
+                case 6:# Найти контакт
+                        name = input('Введите имя: ')
+                        fone = input('Введите номер телефона: ')
+                        with open('text.txt', 'r', encoding='utf-8') as text:
+                                content = text.readlines()
+                                for i in range(len(content)):
+                                        line = content[i].split(',')
+                                        if line[0].strip() == name or line[1].strip() == fone:
+                                                print(f'Найден контакт, имя: {line[0]}, {line[2].strip()} телефон: {line[1]}')
+                                                line = []
+                                        else:
+                                                line = []
+
+                case 7:
+                        print('Удалить контакт')
+                case 8:
+                        print('Выход')
+                        break
